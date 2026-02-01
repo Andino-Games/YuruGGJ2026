481d8508f8de2300079d7090a5bd8c357b82c03a
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2026-02-01 09:54:28 -0500

    Animation

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -14,15 +14,16 @@ namespace Script.Player
         [SerializeField] private float jumpForce;
         [SerializeField] private float acceleraton = 10f;
         [SerializeField] private float decelerator = 10f;
-        [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private Transform groundCheckPoint;
-        [SerializeField] private float groundCheckRadius = 0.2f;
+        SpriteRenderer sp;
+        Animator anim;
         private bool _isGrounded;
 
         private void Awake()
         {
             _controls = new PlayerControls();
             _rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+            sp = GetComponent<SpriteRenderer>();
         }
 
         private void FixedUpdate()
@@ -31,6 +32,24 @@ namespace Script.Player
 
             float targetSpeed = _moveInput.x * speed;
             float learp = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleraton : decelerator;
+            if(_moveInput.x !=0) 
+            {
+                anim.SetBool("Run", true);
+                if(_moveInput.x > 0)
+                {
+                    sp.flipX = false;
+                }
+                else
+                {
+                    sp.flipX = true;
+
+                 }
+            }
+            else
+            {
+                anim.SetBool("Run", false);
+
+            }
 
             float newVelocity = Mathf.Lerp(_rb.linearVelocity.x, targetSpeed, learp * Time.fixedDeltaTime);
             _rb.linearVelocity = new Vector2(newVelocity, _rb.linearVelocity.y);
@@ -40,12 +59,13 @@ namespace Script.Player
                 Jump();
             }
         }
-
+        
         private void Jump()
         {            
             if (_isGrounded)
             {
                 _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                anim.SetBool("Jump", true);
                 _isGrounded = false;
             }
         }
@@ -54,18 +74,11 @@ namespace Script.Player
         {
             if(collision.gameObject.CompareTag("Ground"))
             {
+
+                anim.SetBool("Jump", false);
                 _isGrounded = true;
             }
         }
-
-        private void OnDrawGizmos()
-        {
-            if (!groundCheckPoint) return;
-            
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
-        }
-
         private void OnEnable() => _controls.Player.Enable();
 
         private void OnDisable() => _controls.Player.Disable();
